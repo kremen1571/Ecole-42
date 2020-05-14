a0e5f6fa0ea0df782005284ca0ff4612f7a2a1d9
Kremen1571 <anton.sahar@yandex.com>
2020-05-14 14:27:26 +0300

    add ft_strlcpy.c to libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -28,6 +28,8 @@ void	*ft_memccpy(void *destination, const void *source, int c, size_t n);
 void	*ft_memmove(void *destination, const void *source, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 
 #endif
