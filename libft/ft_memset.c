17d021b7afe8ef2575eaee9db927b20c34630a1a
Kremen1571 <anton.sahar@yandex.com>
2020-05-14 14:28:28 +0300

    add ft_memset.c

---
+++ libft/ft_memset.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: klaronda <klaronda@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/12 12:35:07 by klaronda          #+#    #+#             */
+/*   Updated: 2020/05/12 17:39:15 by klaronda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *destination, int c, size_t n)
+{
+	char	*dest;
+
+	if (!destination)
+	{
+		return (destination);
+	}
+	dest = (char *)destination;
+	while (n > 0)
+	{
+		dest[n - 1] = c;
+		n--;
+	}
+	return (destination);
+}
