c6c0b86a790ee35e367df2433cf5554217cc04f1
Kremen1571 <anton.sahar@yandex.com>
2020-05-15 16:00:41 +0300

    ft_strchr.c

--- libft/libft.h
+++ libft/libft.h
@@ -28,7 +28,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 size_t	ft_strlen(const char *s);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-
+char	*ft_strchr(const char *s, int c);
 
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 
