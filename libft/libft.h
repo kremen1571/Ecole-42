dcd8f89d4c96e78a5e126964319fd5ec8235af5c
Kremen1571 <anton.sahar@yandex.com>
2020-05-15 18:57:19 +0300

    ft_isalpha.c

--- libft/libft.h
+++ libft/libft.h
@@ -33,5 +33,6 @@ char	*ft_strrchr(const char *s, int c);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *nptr);
+int		ft_isalpha(int c);
 
 #endif
